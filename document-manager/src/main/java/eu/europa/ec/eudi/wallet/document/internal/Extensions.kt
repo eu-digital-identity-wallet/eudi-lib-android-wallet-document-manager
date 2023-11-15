5016abab1d50544abcd0dfef7ff75b6d7e49816e
vkanellpoulos <vkanellopoulos@scytales.com>
2023-11-15 13:13:19 +0200

    add extension to convert CBOR nameSpacedData of documents to JSONObject

--- document-manager/src/main/java/eu/europa/ec/eudi/wallet/document/internal/Extensions.kt
+++ document-manager/src/main/java/eu/europa/ec/eudi/wallet/document/internal/Extensions.kt
@@ -20,6 +20,8 @@ import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Build
 import com.upokecenter.cbor.CBORObject
+import com.upokecenter.cbor.CBORType
+import java.util.Base64
 
 @get:JvmSynthetic
 internal val Context.isDeviceSecure: Boolean
@@ -47,4 +49,35 @@ internal fun CBORObject.getEmbeddedCBORObject(): CBORObject {
 @JvmSynthetic
 internal fun ByteArray.withTag24(): ByteArray {
     return CBORObject.FromObjectAndTag(this, 24).EncodeToBytes()
+}
+
+@JvmSynthetic
+internal fun ByteArray.toObject(): Any? {
+    return CBORObject.DecodeFromBytes(this).parse()
+}
+
+private fun CBORObject.parse(): Any? {
+    if (isNull) return null
+    if (isTrue) return true
+    if (isFalse) return false
+
+    return when (this.type) {
+        CBORType.Number -> when {
+            CanValueFitInInt32() -> ToObject(Int::class.java)
+            CanValueFitInInt64() -> ToObject(Long::class.java)
+            else -> ToObject(Double::class.java)
+        }
+        CBORType.Boolean, CBORType.SimpleValue -> isTrue
+        CBORType.ByteString -> Base64.getEncoder().encodeToString(GetByteString())
+        CBORType.TextString -> AsString()
+        CBORType.Array -> values.map { it.parse() }.toList()
+        CBORType.Map -> keys.associate { it.parse() to this[it].parse() }
+        CBORType.Integer -> when {
+            CanValueFitInInt32() -> ToObject(Int::class.java)
+            CanValueFitInInt64() -> ToObject(Long::class.java)
+            else -> ToObject(Double::class.java)
+        }
+        CBORType.FloatingPoint -> ToObject(Float::class.java)
+        else -> null
+    }
 }
