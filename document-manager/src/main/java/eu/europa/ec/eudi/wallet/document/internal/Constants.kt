65b8a97060ba8fd56859e2af26922013f575032e
Marina Ioannou <ioannoum@scytales.com>
2024-03-04 18:51:09 +0200

    Constants file updated, upgrade version to 0.2.3-SNAPSHOT

--- document-manager/src/main/java/eu/europa/ec/eudi/wallet/document/internal/Constants.kt
+++ document-manager/src/main/java/eu/europa/ec/eudi/wallet/document/internal/Constants.kt
@@ -34,33 +34,10 @@ internal const val MDL_DOCTYPE = "org.iso.18013.5.1.mDL"
 @get:JvmSynthetic
 internal const val MDL_NAMESPACE = "org.iso.18013.5.1"
 
-// AAMVA NAMESPACE
-@get:JvmSynthetic
-internal const val AAMVA_NAMESPACE = "org.iso.18013.5.1.aamva"
-
-// Vehicle Registration
-@get:JvmSynthetic
-internal const val MVR_DOCTYPE = "nl.rdw.mekb.1"
-
-@get:JvmSynthetic
-internal const val MVR_NAMESPACE = "nl.rdw.mekb.1"
-
-// COVID
-@get:JvmSynthetic
-internal const val MICOV_DOCTYPE = "org.micov.1"
-
-@get:JvmSynthetic
-internal const val MICOV_VTR_NAMESPACE = "org.micov.vtr.1"
-
-@get:JvmSynthetic
-internal const val MICOV_ATT_NAMESPACE = "org.micov.attestation.1"
-
 @JvmSynthetic
 internal fun Context.docTypeName(docType: String): String? =
     when (docType) {
         EU_PID_DOCTYPE -> resources.getString(R.string.eu_pid_doctype_name)
         MDL_DOCTYPE -> resources.getString(R.string.mdl_doctype_name)
-        MICOV_DOCTYPE -> resources.getString(R.string.micov_doctype_name)
-        MVR_DOCTYPE -> resources.getString(R.string.mvr_doctype_name)
         else -> null
     }
